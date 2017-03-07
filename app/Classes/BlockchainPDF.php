7ebc5f08875609ec44c16dd0d3f52168fd9642da
hotsaucejake <get@hotsaucejake.com>
2017-03-07 19:37:03 +0000

    extend Fpdf with BlockchainPDF class
    - Reason(s):
    
    - Change(s):
    
    - Reference(s):

---
+++ app/Classes/BlockchainPDF.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Classes;
+
+//use Codedge\Fpdf\Facades\Fpdf;
+
+use Codedge\Fpdf\Fpdf\FPDF;
+
+class BlockchainPDF extends Fpdf{
+
+
+   // Page footer
+   function Footer()
+   {
+       // Position at 1.5 cm from bottom
+       $this->SetY(-15);
+       // Arial italic 8
+       $this->SetFont('Courier', 'I', 8);
+       // Page number
+       $this->Cell(0, 10, $this->PageNo(), 0, 0, 'C');
+   }
+
+   function CoverPage($name = 'Anonymous', $email = NULL, $protocol, $title = NULL){
+      $this->SetFont('Courier', 'B', 24);
+      $this->Cell(0, 18, $protocol . ':', 0, 1, 'C');
+      $this->SetFont('Courier', 'B', 18);
+      $this->MultiCell(0, 9, $title, 0, 'C');
+      $this->Ln(9);
+      $this->SetFont('Courier', '', 15);
+      $this->MultiCell(0, 9, $name . "\n" . $email, 0, 'C');
+      $this->Ln(18);
+   }
+
+
+}
--- app/Http/Controllers/PDFGeneratorController.php
+++ app/Http/Controllers/PDFGeneratorController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Codedge\Fpdf\Facades\Fpdf;
+//use Codedge\Fpdf\Facades\Fpdf;
+use App\Classes\BlockchainPDF;
 
 class PDFGeneratorController extends Controller
 {
@@ -15,20 +16,13 @@ class PDFGeneratorController extends Controller
     public function generate(Request $request)
     {
         //dd(request()->all());
-        // echo request('name');
-        // echo request('email-address');
-        // echo request('protocol');
-        // echo request('coin');
-        // echo request('title');
-        Fpdf::AddPage();
-        Fpdf::SetFont('Courier', 'B', 18);
-        Fpdf::Cell(50, 25, 'Name: ' . request('name'), 1, 1, 'C');
-        Fpdf::Cell(50, 25, 'Email: ' . request('email-address'), 1, 1, 'C');
-        Fpdf::Cell(50, 25, 'Protocol: ' . request('protocol'), 1, 1, 'C');
-        Fpdf::Cell(50, 25, 'Coin: ' . request('coin'), 1, 1, 'C');
-        Fpdf::Cell(50, 25, 'Title: ' . request('title'), 1, 1, 'C');
-        Fpdf::Output();
+        // if(request('bitcoin')){ dd(request()->all()); }
 
+        $pdf = new BlockchainPDF();
+
+        $pdf->AddPage();
+        $pdf->CoverPage(request('name'), request('email'), request('protocol'), request('title'));
+        $pdf->Output();
 
     }
 
