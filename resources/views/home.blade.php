1695893d5a09d65ef03962b7ab98aa3d589ecb3d
hotsaucejake <get@hotsaucejake.com>
2017-03-07 19:04:38 +0000

    update form to check for protocol values
    - Reason(s):
    
    - Change(s):
    
    - Reference(s):

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,7 +22,7 @@
 
 <!-- START - Home Part -->
 <section id="home-wrap">
-   
+
    <canvas id="square-canvas" width="" height=""></canvas>
 
    <!-- Stars Overlay - Uncomment the next 3 lines to activate the effect-->
@@ -40,7 +40,7 @@
       <p class="text-intro opacity-0">Want to impress your friends?  Having trouble generating a whitepaper for your next <strike>scam</strike> project?  Seize your blockchain oracle status with this whitepaper generator.</p>
 
       <a href="#" id="open-more-info" data-target="info-wrap" class="action-btn trigger text-intro opacity-0">Get Started</a>
-            
+
    </div> <!-- /. content -->
 
    <!-- Social icons -->
@@ -58,11 +58,11 @@
 <section id="info-wrap">
 
    <div class="hero">
-         
+
       <div class="center-text">
 
          <h3 class="darky">Metachain</h3>
-      
+
          <p><strong>Globally Secured Processes by Immutable Audit Trails on a Pseudo-Anonymous Privacy-Centric Open-Source Next-Generation Data Preservation Smart Contract Prediction Market Network and a Decentralized Peer-to-Peer Electronic Federated Application Platform Model for a Generalized Internet-Level Database Transaction Ledger System by Consensus Protocol Whitepaper Generator</strong></p>
          <p>&nbsp;</p>
          <p>Koinster</p>
@@ -99,11 +99,11 @@
                         </p>
                     </div>
                 </div>
-                
+
                 <div class="col-xs-12 item-map">
                      <p>With any new project, the idea and resulting whitepaper is a burden upon developers looking to push their implementation out the door.  Use this whitepaper generator to take out all of hardwork and guesswork for your next <strike>scam</strike> project.  Use the .pdf to promote your product and profit from its success.</p>
                 </div>
-                
+
                 <div class="col-xs-12 item-map">
                      <p class="small"><strong>Disclaimer:</strong> your impending blockchain oracle status is heavily dependent upon which protocols you choose to include in the whitepaper.  This generator cannot be held liable for any failed implementations.</p>
                 </div>
@@ -125,7 +125,7 @@
 
                 <!-- Full name -->
                 <div class="col-xs-12 col-sm-6 col-lg-6">
-                   
+
                     <div class="form-group">
                        <label for="name" class="small">Full Name</label>
                         <input type="text" id="name" class="form form-control" placeholder="Default: Anonymous" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Default: Anonymous'" name="name" data-name="Name">
@@ -139,10 +139,10 @@
                         <input type="email" id="email" class="form form-control" placeholder="Default: (blank)" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Default: (blank)'" name="email-address" data-name="Email Address">
                     </div>
                 </div>
-                
+
                 <!-- Protocol -->
                 <div class="col-xs-12 col-sm-6 col-lg-6">
-                   
+
                     <div class="form-group">
                        <label for="protocol" class="small">Protocol *</label>
                         <input type="text" id="protocol" class="form form-control" placeholder="Bitcoin, Monero, Ethereum, Metachain, etc.." onfocus="this.placeholder = ''" onblur="this.placeholder = 'Bitcoin, Monero, Ethereum, Metachain, etc..'" name="protocol" data-name="Protocol" required="">
@@ -164,56 +164,56 @@
                         <textarea id="title" class="form textarea form-control" placeholder="Default: Automatically generated based on protocol selections" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Default: Automatically generated based on protocol selections'" name="title" data-name="Title"></textarea>
                     </div>
                 </div>
-                
+
                 <div class="col-xs-12 col-sm-12 col-lg-12">
                    <div class="form-group">
                       <p class="lead text-center" style="color:#4d4d4d;"><strong>Protocols</strong></p>
                     </div>
                 </div>
-                
+
                 <div class="col-xs-12 col-sm-12 col-lg-12 mx-auto">
 
                       <div class="form-check">
                           <label class="form-check-label">
-                            <input class="form-check-input" type="checkbox" id="bitcoin" value="bitcoin" checked=""> <strong>Bitcoin:</strong> A Peer-to-Peer Electronic Cash System
+                            <input class="form-check-input" type="checkbox" id="bitcoin" name="bitcoin" value="1" checked=""> <strong>Bitcoin:</strong> A Peer-to-Peer Electronic Cash System
                           </label>
                      </div>
                      <div class="form-check">
                          <label class="form-check-label">
-                           <input class="form-check-input" type="checkbox" id="lightning" value="lightning"> <strong>The Bitcoin Lightning Network:</strong> Scalable Off-Chain Instant Payments
+                           <input class="form-check-input" type="checkbox" id="lightning" name="lightning" value="1"> <strong>The Bitcoin Lightning Network:</strong> Scalable Off-Chain Instant Payments
                          </label>
                     </div>
                     <div class="form-check">
                         <label class="form-check-label">
-                          <input class="form-check-input" type="checkbox" id="tumblebit" value="tumblebit"> <strong>TumbleBit:</strong> An Untrusted Bitcoin-Compatible Anonymous Payment Hub
+                          <input class="form-check-input" type="checkbox" id="tumblebit" name="tumblebit" value="1"> <strong>TumbleBit:</strong> An Untrusted Bitcoin-Compatible Anonymous Payment Hub
                         </label>
                    </div>
                      <div class="form-check">
                           <label class="form-check-label">
-                            <input class="form-check-input" type="checkbox" id="ethereum" value="ethereum"> <strong>Ethereum:</strong> A Secure Decentralised Generalised Transaction Ledger
+                            <input class="form-check-input" type="checkbox" id="ethereum" name="ethereum" value="1"> <strong>Ethereum:</strong> A Secure Decentralised Generalised Transaction Ledger
                           </label>
                      </div>
                      <div class="form-check">
                           <label class="form-check-label">
-                            <input class="form-check-input" type="checkbox" id="cryptonote" value="cryptonote"> <strong>CryptoNote</strong> v 2.0
+                            <input class="form-check-input" type="checkbox" id="cryptonote" name="cryptonote" value="1"> <strong>CryptoNote</strong> v 2.0
                           </label>
                      </div>
                      <div class="form-check">
                           <label class="form-check-label">
-                            <input class="form-check-input" type="checkbox" id="mimblewimble" value="mimble"> <strong>MimbleWimble</strong>
+                            <input class="form-check-input" type="checkbox" id="mimblewimble" name="mimble" value="1"> <strong>MimbleWimble</strong>
                           </label>
                      </div>
                      <div class="form-check">
                           <label class="form-check-label">
-                            <input class="form-check-input" type="checkbox" id="enigma" value="enigma"> <strong>Enigma:</strong> Decentralized Computation Platform with Guaranteed Privacy
+                            <input class="form-check-input" type="checkbox" id="enigma" name="enigma" value="1"> <strong>Enigma:</strong> Decentralized Computation Platform with Guaranteed Privacy
                           </label>
                      </div>
-                  
-                  
-                  
-                  
-                  
-                  
+
+
+
+
+
+
 
                </div>
 
@@ -221,7 +221,7 @@
             <p>&nbsp;</p>
          <!-- Button submit -->
          <button type="submit" id="valid-form" class="btn btn-color btn-lg btn-block"><strong>GENERATE</strong></button>
-         
+
          <div class="row">
             <div class="col-xs-12 col-sm-12 col-lg-12 no-padding">
                <p class="small text-center" style="color:#4d4d4d;"> The generated .pdf will open in a new window</p>
@@ -230,14 +230,14 @@
 
         </form>
         <!-- END - Contact Form -->
-        
-        <!-- !!! Answer for the contact form is displayed in the next div, do not remove it. -->       
+
+        <!-- !!! Answer for the contact form is displayed in the next div, do not remove it. -->
         <div id="block-answer">
 
          <div id="answer"></div>
 
       </div>
-   
+
    </div> <!-- /. content-form -->
 
    <footer>
@@ -251,7 +251,7 @@
 
 <!-- Button Cross to close the More Informations/Right Part -->
 <div class="command-info-wrap">
-   
+
    <!-- Cross to close -->
    <button class="to-close">
       <i class="icon ion-close-round"></i>
@@ -267,14 +267,14 @@
 <!-- Root element of PhotoSwipe, the gallery. Must have class pswp. -->
 <div class="pswp" tabindex="-1" role="dialog" aria-hidden="true">
 
-    <!-- Background of PhotoSwipe. 
+    <!-- Background of PhotoSwipe.
       It's a separate element as animating opacity is faster than rgba(). -->
     <div class="pswp__bg"></div>
 
     <!-- Slides wrapper with overflow:hidden. -->
     <div class="pswp__scroll-wrap">
 
-        <!-- Container that holds slides. 
+        <!-- Container that holds slides.
             PhotoSwipe keeps only 3 of them in the DOM to save memory.
             Don't modify these 3 pswp__item elements, data is added later on. -->
         <div class="pswp__container">
@@ -313,7 +313,7 @@
             </div>
 
             <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
-                <div class="pswp__share-tooltip"></div> 
+                <div class="pswp__share-tooltip"></div>
             </div>
 
             <button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)">
