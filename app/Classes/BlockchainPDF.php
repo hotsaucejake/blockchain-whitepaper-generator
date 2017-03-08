0e84dcd3b099373957f3dab344ba54b8a02861b8
hotsaucejake <get@hotsaucejake.com>
2017-03-08 01:57:00 +0000

    proof-of-concept
    - Reason(s):
    
    - Change(s):
    
    - Reference(s):

--- app/Classes/BlockchainPDF.php
+++ app/Classes/BlockchainPDF.php
@@ -2,8 +2,6 @@
 
 namespace App\Classes;
 
-//use Codedge\Fpdf\Facades\Fpdf;
-
 use Codedge\Fpdf\Fpdf\FPDF;
 
 class BlockchainPDF extends Fpdf{
@@ -15,20 +13,20 @@ class BlockchainPDF extends Fpdf{
        // Position at 1.5 cm from bottom
        $this->SetY(-15);
        // Arial italic 8
-       $this->SetFont('Courier', 'I', 8);
+       $this->SetFont('Times', 'B', 8);
        // Page number
        $this->Cell(0, 10, $this->PageNo(), 0, 0, 'C');
    }
 
    function CoverPage($name = 'Anonymous', $email = NULL, $protocol, $title = NULL){
-      $this->SetFont('Courier', 'B', 24);
+      $this->SetFont('Times', 'B', 24);
       $this->Cell(0, 18, $protocol . ':', 0, 1, 'C');
-      $this->SetFont('Courier', 'B', 18);
+      $this->SetFont('Times', 'B', 18);
       $this->MultiCell(0, 9, $title, 0, 'C');
       $this->Ln(9);
-      $this->SetFont('Courier', '', 15);
+      $this->SetFont('Times', '', 15);
       $this->MultiCell(0, 9, $name . "\n" . $email, 0, 'C');
-      $this->Ln(18);
+      $this->Ln(24);
    }
 
 
--- app/Http/Controllers/PDFGeneratorController.php
+++ app/Http/Controllers/PDFGeneratorController.php
@@ -18,10 +18,31 @@ class PDFGeneratorController extends Controller
         //dd(request()->all());
         // if(request('bitcoin')){ dd(request()->all()); }
 
-        $pdf = new BlockchainPDF();
+        if (empty(request('name'))) {
+           $name = 'Anonymous';
+        } else {
+           $name = request('name');
+        }
 
+        if (empty(request('title'))) {
+           // NULL title handler
+           $title = 'Globally Secured Processes by Immutable Audit Trails on a Pseudo-Anonymous Privacy-Centric Open-Source Next-Generation Data Preservation Smart Contract Prediction Market Network and a Decentralized Peer-to-Peer Electronic Federated Application Platform Model for a Generalized Internet-Level Database Transaction Ledger System by Consensus Protocol';
+        } else {
+           $title = request('title');
+        }
+
+        $pdf = new BlockchainPDF();
+        $pdf->setTitle(request('protocol'));
         $pdf->AddPage();
-        $pdf->CoverPage(request('name'), request('email'), request('protocol'), request('title'));
+
+        $pdf->CoverPage($name, request('email'), request('protocol'), $title);
+
+        $pdf->SetFont('Times', '', 12);
+        $pdf->MultiCell(0, 6, $title . '.  ' . $title . '.  ' . $title . '.', 0, 'J');
+        $pdf->MultiCell(0, 6, $title . '.  ' . $title . '.  ' . $title . '.', 0, 'J');
+        $pdf->MultiCell(0, 6, $title . '.  ' . $title . '.  ' . $title . '.', 0, 'J');
+        $pdf->MultiCell(0, 6, $title . '.  ' . $title . '.  ' . $title . '.', 0, 'J');
+
         $pdf->Output();
 
     }
